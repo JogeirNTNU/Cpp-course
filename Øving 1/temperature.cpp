e747682ec3d33b6e28c4a74efe1f98014a00b970
JogeirNTNU <jjliavaa@stud.ntnu.no>
2026-08-27 14:45:19 +0200

    remove deprecated code

--- Øving 1/temperature.cpp
+++ Øving 1/temperature.cpp
@@ -27,27 +27,4 @@ int main() {
   std::cout << "Under 10: " << lessThan10 << std::endl;
   std::cout << "Between 10 and 20: " << between10And20 << std::endl;
   std::cout << "Over 20: " << over20 << std::endl;
-
-  // std::vector<double> temperatures{};
-  //
-  // std::cout << "Insert 5 temperatures" << std::endl;
-  //
-  // for (int i = 0; i < length; i++) {
-  //   std::cout << "Temperature number " << i;
-  //   std::cin >> temperatures[i];
-  // }
-  //
-  // std::vector<double> lessThan10{};
-  // std::vector<double> between10And20{};
-  // std::vector<double> over20{};
-  //
-  // for (int i = 0; i < length; i++) {
-  //   if (temperatures[i] < 10) {
-  //     lessThan10.push_back(temperatures[i]);
-  //   } else if (temperatures[i] >= 10 && temperatures[i] <= 20) {
-  //     between10And20.push_back(temperatures[i]);
-  //   } else {
-  //     over20.push_back(temperatures[i]);
-  //   }
-  // }
 }
